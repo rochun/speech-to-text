baf13e29cb311ba39477acf1cd58521e1bc35f5c
rochun <royce.h.chun@gmail.com>
2023-06-04 15:57:38 -0700

    add react routers and routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,25 @@
 import { useState } from 'react';
-import {Register} from './views/Register';
+import { Route, Routes } from "react-router-dom";
+import { ProtectedRoute } from "./components/ProtectedRoute";
+import { Register } from './views/Register';
+import { Home } from "./views/Home";
+import { Login } from './views/Login';
+import { NavBar } from './components/NavBar';
+import { NavbarLayout } from './components/NavbarLayout';
 
 function App() {
-
   return (
     <>
-      <Register/>
+      <Routes>
+        <Route element={<ProtectedRoute />}>
+          <Route element={<NavbarLayout/>}>
+            <Route path="/" element={<Home />} />
+            <Route path="/home" element={<Home />} />
+          </Route>
+        </Route>
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
     </>
   )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
+import { BrowserRouter } from "react-router-dom";
 import AuthProvider from './context/AuthProvider';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <AuthProvider>
-      <App />
-    </AuthProvider>
+    <BrowserRouter>
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </BrowserRouter>
   </React.StrictMode>,
 )
