7f04d2a40e7499532275fecd4d8cc4ca5c16590f
rochun <royce.h.chun@gmail.com>
2023-06-06 18:53:26 -0700

    implement Notes component for listing

---
+++ src/components/Notes.tsx
@@ -0,0 +1,61 @@
+import * as React from 'react';
+import ListItem from '@mui/material/ListItem';
+import ListItemText from '@mui/material/ListItemText';
+import ListItemButton from '@mui/material/ListItemButton';
+import IconButton from '@mui/material/IconButton';
+import DeleteIcon from '@mui/icons-material/Delete';
+import Typography from '@mui/material/Typography';
+
+interface NotesProps {
+  note: {[x: string]: any;} | null | undefined;
+  handleDelete: (note_id: string) => Promise<void>;
+  setSelectedNote: React.Dispatch<React.SetStateAction<{[x: string]: any;} | null | undefined>>;
+  selected: boolean;
+}
+
+export const Note = ({ note, handleDelete, setSelectedNote, selected } : NotesProps) => {
+
+  return (
+    <ListItem
+      secondaryAction={
+        <IconButton edge="end" aria-label="delete">
+          <DeleteIcon onClick={() => {handleDelete(note?.id)}}/>
+        </IconButton>
+      }
+      divider={true}
+      sx={{
+        '&:hover': {
+          backgroundColor: '#D3D3D3',
+        },
+      }}
+      disablePadding
+    >
+      <ListItemButton
+        sx={{
+          height: '100%',
+        }}
+        onClick={() => {
+          console.log(note);
+          setSelectedNote(note);
+        }}
+        selected={selected}
+      >
+        <ListItemText
+          primary={
+            <React.Fragment>
+              <Typography
+                sx={{ display: 'inline' }}
+                component="span"
+                variant="body2"
+                color="text.primary"
+              >
+                {new Date(note.updated_at).toDateString()}
+              </Typography>
+              {note?.note ? ' - '+ note.note.substr(0, 100) + '...' : null}
+            </React.Fragment>
+          }
+        />
+      </ListItemButton>
+    </ListItem>
+  );
+}
---
+++ src/components/NotesList.tsx
@@ -0,0 +1,36 @@
+import { Note } from './Notes';
+import List from '@mui/material/List';
+
+interface NotesListProps {
+  notes: {[x: string]: any;}[] | null | undefined;
+  handleDelete: (note_id: string) => Promise<void>;
+  setSelectedNote: React.Dispatch<React.SetStateAction<{[x: string]: any;} | null | undefined>>;
+  selectedNote: {[x: string]: any;} | null | undefined;
+}
+export const NotesList = ({notes, handleDelete, setSelectedNote, selectedNote} : NotesListProps ) => {
+  return (
+    <List sx={{
+      width: '100%',
+      maxWidth: 360,
+      bgcolor: 'background.paper',
+      overflow: 'auto',
+      height: '30em',
+      borderStyle: 'solid',
+    }}>
+      {notes?.length ? notes.map((note: any) => {
+          let selected = false;
+          if (selectedNote?.id === note.id) {
+            selected = true;
+          }
+          return (
+            <Note
+              note={note}
+              handleDelete={handleDelete}
+              setSelectedNote={setSelectedNote}
+              selected={selected}
+            />)
+      })
+      : null}
+    </List>
+  );
+};
